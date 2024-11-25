3c112029d751c386cc613f52921386fbc13bbdba
afriansyah <alfinafriansyah68@gmail.com>
2024-11-25 15:12:59 +0700

    JS13.2-Praktikum2

--- JS13/lib/Connection.php
+++ JS13/lib/Connection.php
@@ -1,14 +1,37 @@
 <?php  
-    $username = 'root'; 
-    $password = ''; 
-    $database = 'dasar_web'; 
+    $use_driver = 'sqlsrv'; // mysql atau sqlsrv 
  
-    try{ 
-        $db = new mysqli('localhost', $username, $password, $database); 
-        if($db->connect_error){ 
-            die('Connection DB failed: ' . $db->connect_error); 
+    $host     = 'LAPTOP-7A3IMJFG'; 
+    $username = ''; //'sa'; 
+    $password = ''; 
+    $database = 'master'; 
+    $db; 
+ 
+    if($use_driver == 'mysql'){    
+        try{ 
+            $db = new mysqli('localhost', $username, $password, $database); 
+            if($db->connect_error){ 
+                die('Connection DB failed: ' . $db->connect_error); 
+            } 
+        }catch(Exception $e){ 
+            die($e->getMessage()); 
         } 
-    }catch(Exception $e){ 
-        die($e->getMessage()); 
+    } else if($use_driver == 'sqlsrv'){ 
+        $credential = [ 
+            'Database' => $database,  
+            'UID' => $username,  
+            'PWD' => $password 
+        ]; 
+         
+        try{ 
+            $db = sqlsrv_connect($host, $credential); 
+ 
+            if (!$db){ 
+                $msg = sqlsrv_errors(); 
+                die($msg[0]['message']); 
+            } 
+        }catch(Exception $e){ 
+            die($e->getMessage()); 
+        } 
     } 
 ?> 
